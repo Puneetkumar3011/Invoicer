a7b055bedad4aa99d9aa79ff29bef3b09c2e04fe
puneet3011@gmail.com <puneet3011@gmail.com>
2016-11-03 22:11:31 -0400

    Add files

---
+++ client/controllers/dashboard.js
@@ -0,0 +1,9 @@
+(function(){
+	'use strict';
+	var myApp = angular.module("myApp");
+
+	myApp.controller('DashboardController', ['$scope', '$http','$location', function($scope, $http,$location){
+		console.log('Dashboard Controller Initialized...');
+	}]);
+
+})();
